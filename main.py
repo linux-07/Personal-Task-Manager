1d3139874975b137485f02d0d96c2b2dbea6babf
arnavnagpurkar <arnav.nagpurkar@gmail.com>
2023-09-05 19:51:12 +0530

    Added error handling in load_tasks() function

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ def save_tasks():
             file.write(f"Description: {task['description']}\n")
             file.write(f"Due Date: {task['due_date']}\n")
             file.write(f"Status: {'completed' if task['completed'] else 'pending'}\n\n") # --> Short hand if-else used in the line
+
+def load_tasks():
+    try:
+        with open("tasks.txt", 'r') as file:
+            pass
+    except:
+        raise FileNotFoundError
