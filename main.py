c741317d4b998785d8f346be8e300a0bd79151d0
arnavnagpurkar <arnav.nagpurkar@gmail.com>
2023-09-05 19:48:24 +0530

    Written Code for save_tasks() function

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from datetime import datetime
 
 # Create Empty Tasks list
 tasks = []
+
+def save_tasks():
+    with open("tasks.txt", 'w') as file:
+        for task in tasks:
+            file.write(f"Title: {task['title']}\n")
+            file.write(f"Description: {task['description']}\n")
+            file.write(f"Due Date: {task['due_date']}\n")
+            file.write(f"Status: {'completed' if task['completed'] else 'pending'}\n\n") # --> Short hand if-else used in the line
